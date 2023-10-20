29e0b6bc4488c83cff11a819d4948e3d8a837e78
God Reaper <brajutso@gmail.com>
2023-10-20 08:19:09 +0530

    created components and created navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '../styles/globals.css';
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
-const Myapp = ({Component, pageProps}) => <Component {...pageProps} />;
+const Myapp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/2ee570cc82.js" crossorigin="anonymous"></Script>
+  </ThemeProvider>
+);
 
 export default Myapp;
---
+++ pages/createnft.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const createnft = () => {
+  return (
+    <div>createnft</div>
+  )
+}
+
+export default createnft
