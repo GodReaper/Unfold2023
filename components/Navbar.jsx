7107c66a9d28ff3f0943ec3a2a6add2a95d51b08
God Reaper <brajutso@gmail.com>
2023-10-20 08:52:37 +0530

    made navbar mobile responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./";
 import images from "../assets";
-import { isMobile } from "web3modal";
+
 
 const MenuItems = (isMobile, active, setActive) => {
   const generateLink = (i) => {
@@ -66,6 +66,7 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const [active, setActive] = useState("Explore NFTs");
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
@@ -125,7 +126,37 @@ const Navbar = () => {
       </div>
 
       <div className="hidden md:flex ml-2">
-        
+        {isOpen ? (
+            <Image src = {images.cross}
+            objectFit="contain"
+            width={20}
+            height={20}
+            alt="cross"
+            onClick={() => setIsOpen(false)}
+            className={theme === 'light' && 'filter invert' } />
+        ) : (
+            <Image src = {images.menu} 
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="hamburger"
+            onClick={() => setIsOpen(true)}
+            className={theme === 'light' && 'filter invert' }
+            />
+        )
+
+        }
+        {isOpen && (
+            <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+                <div className="flex-1 p-4">
+                    <MenuItems active={active} setActive={setActive} isMobile/>
+                </div>
+                <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+                    <ButtonGroup setActive={setActive} router={router} />
+                </div>
+            </div>
+        )}
+
       </div>
     </nav>
   );
